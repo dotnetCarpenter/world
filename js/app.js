85f20c4349acee415b0cc21d227ccc1862d686f3
dotnetCarpenter <jon.ronnenberg@gmail.com>
2017-04-24 21:39:58 +0200

    a little formatting

--- js/app.js
+++ js/app.js
@@ -2,51 +2,51 @@
 
 !(function(win, doc) {
 
-	var world = SVG("worldmap")
-	var controls = [$(".info-box__button_in"), $(".info-box__button_out")]
-	var zoomFactorOut = displayZoomLevel($(".info-box__zoom-factor"))
-	var currentZoomLevel = 1
+  var world = SVG("worldmap")
+  var controls = [$(".info-box__button_in"), $(".info-box__button_out")]
+  var zoomFactorOut = displayZoomLevel($(".info-box__zoom-factor"))
+  var currentZoomLevel = 1
 
-	main()
+  main()
 
-	function main() {
-		zoomFactorOut(currentZoomLevel)	
+  function main() {
+    zoomFactorOut(currentZoomLevel)	
 
-		world.panZoom({ zoomFactor: 0.2 })
+    world.panZoom({ zoomFactor: 0.2 })
 
-		world.on("zoom", function(z) {
-			currentZoomLevel = z.detail.zoomLevel
-			zoomFactorOut(currentZoomLevel)
-		})
+    world.on("zoom", function(z) {
+      currentZoomLevel = z.detail.zoomLevel
+      zoomFactorOut(currentZoomLevel)
+    })
 
-		SVG.on(controls[0], "click", function() {
-			var p = getCenterPoint(world)
+    SVG.on(controls[0], "click", function() {
+      var p = getCenterPoint(world)
 
-			world.zoom(0.5, p, { duration: 250, easing: "<>" })
-		})
+      world.zoom(0.5, p, { duration: 250, easing: "<>" })
+    })
 
-		SVG.on(controls[1], "click", function() {
-			var p = getCenterPoint(world)
+    SVG.on(controls[1], "click", function() {
+      var p = getCenterPoint(world)
 
-			world.zoom(1.5, p, { duration: 200, easing: "<>" })
-		})
-	}
+      world.zoom(1.5, p, { duration: 200, easing: ">" })
+    })
+  }
 
-	function $(query, el) {
-		el = el || document
-		return document.querySelector(query)
-	}
+  function $(query, el) {
+    el = el || document
+    return document.querySelector(query)
+  }
 
-	function displayZoomLevel(out) {
-		return function(level) {
-			out.textContent = level.toFixed(2)
-		}
-	}
+  function displayZoomLevel(out) {
+    return function(level) {
+      out.textContent = level.toFixed(2)
+    }
+  }
 
-	function getCenterPoint(element) {
-		var size = world.node.getBoundingClientRect()
-		return world.point(size.width / 2,
-												size.height/ 2)
-	}
+  function getCenterPoint(element) {
+    var size = element.node.getBoundingClientRect()
+    return element.point(size.width / 2,
+                         size.height/ 2)
+  }
 
 }(window, document))
