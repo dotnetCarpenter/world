cb6836003d7f7b6b0d58763003f75586e3ce1530
dotnetCarpenter <jon.ronnenberg@gmail.com>
2017-05-21 00:22:14 +0200

    not sure what the last commit was from - this is @Fuzzyma's changes

--- js/lib/svg.panzoom.js
+++ js/lib/svg.panzoom.js
@@ -1,196 +1,174 @@
 (function() {
 
-'use strict'
+var normalizeEvent = function(ev) {
+  if(!ev.touches) {
+    ev.touches = [{clientX: ev.clientX, clientY: ev.clientY}]
+  }
 
-var normalizeEvent = function normalizeEvent(ev) {
-	if(!ev.touches) {
-		ev.touches = [{clientX: ev.clientX, clientY: ev.clientY}]
-	}
-
-	return [].slice.call(ev.touches)
+  return [].slice.call(ev.touches)
 }
 
-var getCenterPoint = function getCenterPoint(element) {
-	var size = element.node.getBoundingClientRect()
-	return element.point(size.width / 2,
-						 size.height/ 2)
-}
+SVG.extend(SVG.Doc, SVG.Nested, {
 
-SVG.extend(SVG.Doc, SVG.Nested, SVG.FX, {
+  panZoom: function(options) {
+    options = options || {}
+    var zoomFactor = options.zoomFactor || 0.03
 
-	panZoom: function(options) {
-		options = options || {}
+    var lastP, lastTouches, zoomInProgress = false
 
-		var zoomFactor = options.zoomFactor || 0.03
+    var wheelZoom = function(ev) {
+      ev.preventDefault()
 
-		var lastP, lastTouches, zoomInProgress = false, lastCall = +new Date()
+      var zoomAmount = this.zoom() - zoomFactor * ev.deltaY/Math.abs(ev.deltaY)
+        , p = this.point(ev.clientX, ev.clientY)
 
-		var wheelZoom = function(ev) {
-			ev.preventDefault()
+      this.zoom(zoomAmount, p)
+    }
 
-			var zoomAmount = ev.deltaY/Math.abs(ev.deltaY) * zoomFactor + 1
+    var pinchZoomStart = function(ev) {
+      lastTouches = normalizeEvent(ev)
 
-			var p = this.point(ev.clientX, ev.clientY)
+      if(lastTouches.length < 2 || zoomInProgress) return
+      ev.preventDefault()
 
-			this.zoom(zoomAmount, p)
-		}
+      panStop.call(this, ev)
 
-		var pinchZoomStart = function(ev) {
-			lastTouches = normalizeEvent(ev)
+      zoomInProgress = true
+      SVG.on(document, 'touchmove', pinchZoom, this, {passive:false})
+      SVG.on(document, 'touchend', pinchZoomStop, this, {passive:false})
+    }
 
-			if(lastTouches.length < 2 || zoomInProgress) return
-			ev.preventDefault()
+    var pinchZoomStop = function(ev) {
+      ev.preventDefault()
+      zoomInProgress = false
 
-			panStop.call(this, ev)
+      SVG.off(document,'touchmove', pinchZoom)
+      SVG.off(document,'touchend', pinchZoomStop)
+      this.on('touchstart', pinchZoomStart)
 
-			zoomInProgress = true
-			SVG.on(document, 'touchmove', pinchZoom, this, {passive:false})
-			SVG.on(document, 'touchend', pinchZoomStop, this, {passive:false})
+      if(ev.touches.length > 0) panStart.call(this, ev)
+    }
 
-			//TODO: when to fire zoom?
-			this.fire('zoomStart')
-		}
+    var pinchZoom = function(ev) {
+      ev.preventDefault()
+      var currentTouches = normalizeEvent(ev)
 
-		var pinchZoomStop = function(ev) {
-			ev.preventDefault()
-			zoomInProgress = false
+      var lastDelta = Math.sqrt((lastTouches[0].clientX - lastTouches[1].clientX)**2 + (lastTouches[0].clientY - lastTouches[1].clientY)**2)
+      var currentDelta = Math.sqrt((currentTouches[0].clientX - currentTouches[1].clientX)**2 + (currentTouches[0].clientY - currentTouches[1].clientY)**2)
 
-			SVG.off(document,'touchmove', pinchZoom)
-			SVG.off(document,'touchend', pinchZoomStop)
-			this.on('touchstart', pinchZoomStart)
+      var zoomAmount = lastDelta/currentDelta
 
-			if(ev.touches.length > 0) panStart.call(this, ev)
-		}
+      var currentFocus = {
+        x: currentTouches[0].clientX + 0.5 * (currentTouches[1].clientX - currentTouches[0].clientX),
+        y: currentTouches[0].clientY + 0.5 * (currentTouches[1].clientY - currentTouches[0].clientY)
+      }
 
-		var pinchZoom = function(ev) {
-			ev.preventDefault()
-			var currentTouches = normalizeEvent(ev)
-
-			var lastDelta = Math.sqrt((lastTouches[0].clientX - lastTouches[1].clientX)**2 + (lastTouches[0].clientY - lastTouches[1].clientY)**2)
-			var currentDelta = Math.sqrt((currentTouches[0].clientX - currentTouches[1].clientX)**2 + (currentTouches[0].clientY - currentTouches[1].clientY)**2)
-
-			var zoomAmount = lastDelta/currentDelta
-
-			var currentFocus = {
-				x: currentTouches[0].clientX + 0.5 * (currentTouches[1].clientX - currentTouches[0].clientX),
-				y: currentTouches[0].clientY + 0.5 * (currentTouches[1].clientY - currentTouches[0].clientY)
-			}
-
-			var lastFocus = {
-				x: lastTouches[0].clientX + 0.5 * (lastTouches[1].clientX - lastTouches[0].clientX),
-				y: lastTouches[0].clientY + 0.5 * (lastTouches[1].clientY - lastTouches[0].clientY)
-			}
+      var lastFocus = {
+        x: lastTouches[0].clientX + 0.5 * (lastTouches[1].clientX - lastTouches[0].clientX),
+        y: lastTouches[0].clientY + 0.5 * (lastTouches[1].clientY - lastTouches[0].clientY)
+      }
 
 
-			var p = this.point(currentFocus.x, currentFocus.y)
-			var focusP = this.point(2*currentFocus.x-lastFocus.x, 2*currentFocus.y-lastFocus.y)
-			var b = new SVG.Box(this.viewbox()).transform(
-				new SVG.Matrix()
-					.translate(p.x, p.y)
-					.scale(zoomAmount, 0, 0)
-					.translate(-focusP.x, -focusP.y)
-			)
+      var p = this.point(currentFocus.x, currentFocus.y)
+      var focusP = this.point(2*currentFocus.x-lastFocus.x, 2*currentFocus.y-lastFocus.y)
+      var b = new SVG.Box(this.viewbox()).transform(
+        new SVG.Matrix()
+          .translate(p.x, p.y)
+          .scale(zoomAmount, 0, 0)
+          .translate(-focusP.x, -focusP.y)
+      )
 
 
-			this.viewbox(b)
+      this.viewbox(b)
 
-			lastTouches = currentTouches
+      lastTouches = currentTouches
+    }
 
-			//TODO: when to fire zoom?
-			this.fire('zoomEnd')
-		}
+    var panStart = function(ev) {
+      ev.preventDefault()
 
-		var panStart = function(ev) {
-			ev.preventDefault()
+      this.off('mousedown', panStart)
+      this.off('touchstart', panStart)
 
-			this.off('mousedown', panStart)
-			this.off('touchstart', panStart)
+      lastTouches = normalizeEvent(ev)
 
-			lastTouches = normalizeEvent(ev)
+      if(zoomInProgress) return
 
-			if(zoomInProgress) return
+      lastP = {x: lastTouches[0].clientX, y: lastTouches[0].clientY }
 
-			lastP = {x: lastTouches[0].clientX, y: lastTouches[0].clientY }
+      SVG.on(document, 'mousemove', panning, this, {passive:false})
+      SVG.on(document, 'touchmove', panning, this, {passive:false})
+      SVG.on(document, 'mouseup', panStop, this, {passive:false})
+      SVG.on(document, 'touchend', panStop, this, {passive:false})
+    }
 
-			SVG.on(document, 'mousemove', panning, this, {passive:false})
-			SVG.on(document, 'touchmove', panning, this, {passive:false})
-			SVG.on(document, 'mouseup', panStop, this, {passive:false})
-			SVG.on(document, 'touchend', panStop, this, {passive:false})
-		}
+    var panStop = function(ev) {
+      ev.preventDefault()
 
-		var panStop = function(ev) {
-			ev.preventDefault()
+      SVG.off(document,'mousemove', panning)
+      SVG.off(document,'touchmove', panning)
+      SVG.off(document,'mouseup', panStop)
+      SVG.off(document,'touchend', panStop)
+      this.on('mousedown', panStart)
+      this.on('touchstart', panStart)
+    }
 
-			SVG.off(document,'mousemove', panning)
-			SVG.off(document,'touchmove', panning)
-			SVG.off(document,'mouseup', panStop)
-			SVG.off(document,'touchend', panStop)
-			this.on('mousedown', panStart)
-			this.on('touchstart', panStart)
-		}
+    var panning = function(ev) {
+      ev.preventDefault()
+      var currentTouches = normalizeEvent(ev)
 
-		var panning = function(ev) {
-			ev.preventDefault()
-			var currentTouches = normalizeEvent(ev)
+      var currentP = {x: currentTouches[0].clientX, y: currentTouches[0].clientY }
+        , p1 = this.point(currentP.x, currentP.y)
+        , p2 = this.point(lastP.x, lastP.y)
+        , deltaP = [p2.x - p1.x, p2.y - p1.y]
+        , box = new SVG.Box(this.viewbox()).transform(new SVG.Matrix().translate(deltaP[0], deltaP[1]))
 
-			var currentP = {x: currentTouches[0].clientX, y: currentTouches[0].clientY }
-				, p1 = this.point(currentP.x, currentP.y)
-				, p2 = this.point(lastP.x, lastP.y)
-				, deltaP = [p2.x - p1.x, p2.y - p1.y]
-				, box = new SVG.Box(this.viewbox()).transform(new SVG.Matrix().translate(deltaP[0], deltaP[1]))
+      this.viewbox(box)
+      lastP = currentP
+    }
 
-			this.viewbox(box)
-			lastP = currentP
-		}
+    this.on('wheel', wheelZoom)
+    this.on('touchstart', pinchZoomStart, this, {passive:false})
+    this.on('mousedown', panStart, this, {passive:false})
+    this.on('touchstart', panStart, this, {passive:false})
 
-		this.on('wheel', wheelZoom)
-		this.on('touchstart', pinchZoomStart, this, {passive:false})
-		this.on('mousedown', panStart, this, {passive:false})
-		this.on('touchstart', panStart, this, {passive:false})
+    return this
 
-		return this
+  },
 
-	},
+  zoom: function(level, point) {
+    var style = window.getComputedStyle(this.node)
+      , width = parseFloat(style.getPropertyValue('width'))
+      , height = parseFloat(style.getPropertyValue('height'))
+      , v = this.viewbox()
+      , zoomX = width / v.width
+      , zoomY = height / v.height
+      , zoom = Math.min(zoomX, zoomY)
 
-	zoom: function(zoomlevel, point) {
-		var isAnimating = this instanceof SVG.FX
-		// get target in case of the fx module, otherwise reference this
-		var target = isAnimating ? this.target() : this
+    if(level == null) {
+      return zoom
+    }
 
-		if(arguments.length === 0) return target.zoomLevel()
+    var zoomAmount = (zoom / level)
 
-		if(point == null) point = getCenterPoint(target)
+    point = point || new SVG.Point(width/2 / zoomX + v.x, height/2 / zoomY + v.y)
 
-		target.fire('zoomStart')
+    var box = new SVG.Box(v)
+      .transform(new SVG.Matrix()
+        .scale(zoomAmount, point.x, point.y)
+      )
 
-		var b = new SVG.Box(target.viewbox())
-			.transform(new SVG.Matrix()
-					.scale(zoomlevel, point.x, point.y))
+    if(this.fire('zoom', {level: level, box: box, focus: point}).event().defaultPrevented)
+      return this
 
-		this.viewbox(b)
-
-		//TODO: when to fire zoom?
-		if(isAnimating) this.after(target.fire.bind(target, 'zoomEnd'))
-		else target.fire('zoomEnd')
-
-		return this
-	},
-
-	zoomToOne: function(point) {
-		// get target in case of the fx module, otherwise reference this
-		var target = this instanceof SVG.FX ? this.target() : this
-
-		this.zoom(target.zoomLevel(), point)
-
-		return this
-	},
-
-	zoomLevel: function(zoomlevel) {
-		if(zoomlevel != null) return this.zoom(zoomlevel)
-		else return this.bbox().width / this.viewbox().width
-	}
-
+    return this.viewbox(box)
+  },
 })
 
+SVG.extend(SVG.FX, {
+  zoom: function(level) {
+    return this.add('zoom', new SVG.Number(level))
+  }
+})
 
 })()
